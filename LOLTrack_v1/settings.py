96629b734527d9f6fe6d010ce120516029220d3a
luisthatguyIZI <C19403516@mytudublin.ie>
2023-01-31 16:14:15 +0000

    base & index page. some simple url routing

--- LOLTrack_v1/settings.py
+++ LOLTrack_v1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g1@!ds#_wv)y%=g)yqq$8#qpc5$-6lre*+bxxuxx5!p+jjts#k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
