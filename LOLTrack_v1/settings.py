99a73287e58312f162ca24bd5fe0c236cc223074
luisthatguyIZI <C19403516@mytudublin.ie>
2023-03-22 15:30:17 +0000

    update

--- LOLTrack_v1/settings.py
+++ LOLTrack_v1/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'LOLTrack_v1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'main',
+        'USER': 'luis',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
